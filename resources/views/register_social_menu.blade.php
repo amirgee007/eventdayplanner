e59cf39f4160c99237c87a438d7868e6283f2113
Amir SeerSol <amirgee007@yahoo.com>
2018-03-30 02:46:12 +0500

    done

--- resources/views/register_social_menu.blade.php
+++ resources/views/register_social_menu.blade.php
@@ -1,7 +1,11 @@
     <div class="jingalala">
         Or register with
+        <a href="{{ route('auth.getSocialAuth','facebook') }}" class="fa fa-facebook" style="background: #3B5998;
         color: white;"></a>
+        <a href="{{ route('auth.getSocialAuth','twitter') }}" class="fa fa-twitter" style="background: #55ACEE;
         color: white;"></a>
+        <a href="{{ route('auth.getSocialAuth','google') }}" class="fa fa-google" style="background: #dd4b39;
         color: white;"></a>
+        <a href="{{ route('auth.getSocialAuth','linkedin') }}" class="fa fa-linkedin" style="background: #007bb5;
         color: white;"></a>
     </div>
