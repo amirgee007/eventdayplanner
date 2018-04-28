83230cf3287905263bb23f1f0580d7f57894ba43
Amir SeerSol <amirgee007@yahoo.com>
2018-04-29 02:32:04 +0500

    messages

--- resources/views/messenger/index.blade.php
+++ resources/views/messenger/index.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.eventday')
-
+@section('title')
+    My Messages
+    @parent
+@stop
 @section('content')
 
 <section class="mainContainer">
@@ -8,8 +11,6 @@
       <div class="row">
       @include('usermenu')
       <div class="col-sm-12"><h3>My Messages</h3></div>
-     
-      
       </div>
     @if (Session::has('error_message'))
         <div class="alert alert-danger" role="alert">
@@ -20,7 +21,7 @@
         @foreach($threads as $thread)
         <?php $class = $thread->isUnread($currentUserId) ? 'alert-info' : ''; ?>
         <div class="media alert {{ $class }}">
-            <h4 class="media-heading">{!! url('messages/' . $thread->id, $thread->subject) !!}</h4>
+            <h4 class="media-heading">{!! url('messages/' . $thread->id) !!}</h4>
             <p>{!! $thread->latestMessage->body !!}</p>
             <p><small><strong>Sender:</strong> {{ $thread->creator()->first_name }} {{ $thread->creator()->last_name }}</small></p>
 <!--             <p><small><strong>Participants:</strong> {{ $thread->participantsString(Auth::id()) }}</small></p>
